7476d60cb6970a3cf422864481e729243b5d2a8d
Hunter Bossetto <hbstudentswic@gmail.com>
2021-05-06 13:51:16 -0500

    feat: :sparkles: Implement Table functionality
    Implenment funtionality to allow the data in the feilds to be brought into the table
    
    n/a

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
+function addInfoRow() {
+  const gamename = document.querySelector('#game-nam').value;
+  const gamesystem = document.querySelector('#game-sys').value;
+  const pricepayed = document.querySelector('#price-payed').value;
+  const valuedprice = document.querySelector('#valued-price').value;
+  const storename = document.querySelector('#store-name').value;
 
+  const table = document.querySelector('table');
+
+  const newRow = table.insertRow(table.rows.length);
+
+  const gnamecell = newRow.insertCell(0);
+  const gsystcell = newRow.insertCell(1);
+  const ppricecell = newRow.insertCell(2);
+  const vpricecell = newRow.insertCell(3);
+  const snamecell = newRow.insertCell(4);
+
+  gnamecell.innerHTML = gamename;
+  gsystcell.innerHTML = gamesystem;
+  ppricecell.innerHTML = pricepayed;
+  vpricecell.innerHTML = valuedprice;
+  snamecell.innerHTML = storename;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+  addInfoRow();
+});
