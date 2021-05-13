4254419967987ee44c11b9cad4b4006b96371ebe
Hunter Bossetto <hbstudentswic@gmail.com>
2021-05-13 13:28:29 -0500

    feat: :sparkles: Create an array with defualt objects
    Create the array that will be used to store 2 defualt games and will have new games added to it.
    
    n/a

--- scripts.js
+++ scripts.js
@@ -1,28 +1,16 @@
-function createRow() {
-  const gamename = document.querySelector('#game-nam').value;
-  const gamesystem = document.querySelector('#game-sys').value;
-  const pricepayed = document.querySelector('#price-payed').value;
-  const valuedprice = document.querySelector('#valued-price').value;
-  const storename = document.querySelector('#store-name').value;
-
-  const table = document.querySelector('table');
-
-  const newRow = table.insertRow(table.rows.length);
-
-  const gnamecell = newRow.insertCell(0);
-  const gsystcell = newRow.insertCell(1);
-  const ppricecell = newRow.insertCell(2);
-  const vpricecell = newRow.insertCell(3);
-  const snamecell = newRow.insertCell(4);
-
-  gnamecell.innerHTML = gamename;
-  gsystcell.innerHTML = gamesystem;
-  ppricecell.innerHTML = pricepayed;
-  vpricecell.innerHTML = valuedprice;
-  snamecell.innerHTML = storename;
-}
-
-document.querySelector('form').addEventListener('submit', event => {
-  event.preventDefault();
-  createRow();
-});
+const gameList = [
+  {
+    gametitle: 'Tetris',
+    systemname: 'Game Boy',
+    pricepayed: '$7',
+    valuedprice: '$9',
+    storename: 'Ebay',
+  },
+  {
+    gametitle: 'Ms. Pac-Man',
+    systemname: 'Sega Genesis',
+    pricepayed: '$10',
+    valuedprice: '$6',
+    storename: 'Slackers',
+  },
+];
