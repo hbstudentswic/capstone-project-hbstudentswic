4556a16a310a931afb1477b6c573d5caed9c29b7
Hunter Bossetto <hbstudentswic@gmail.com>
2021-05-15 19:04:07 -0500

    feat: :art: Create and implement export/import functionality for the gameList array
    Create and implement export/import functionality for the gameList array
    
    n/a

---
+++ data.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    gametitle: 'Tetris',
+    systemname: 'Game Boy',
+    pricepayed: '7',
+    valuedprice: '9',
+    storename: 'Ebay',
+  },
+  {
+    gametitle: 'Ms. Pac-Man',
+    systemname: 'Sega Genesis',
+    pricepayed: '10',
+    valuedprice: '6',
+    storename: 'Slackers',
+  },
+];
--- scripts.js
+++ scripts.js
@@ -1,19 +1,4 @@
-const gameList = [
-  {
-    gametitle: 'Tetris',
-    systemname: 'Game Boy',
-    pricepayed: '7',
-    valuedprice: '9',
-    storename: 'Ebay',
-  },
-  {
-    gametitle: 'Ms. Pac-Man',
-    systemname: 'Sega Genesis',
-    pricepayed: '10',
-    valuedprice: '6',
-    storename: 'Slackers',
-  },
-];
+import gameList from './data.js';
 
 const tbody = document.querySelector('tbody');
 const template = document.querySelector('template');
