fce2db7f597a3e4f449ff81652b12e8a3c656858
Hunter Bossetto <hbstudentswic@gmail.com>
2021-05-16 14:04:20 -0500

    docs: :memo: Add some comments about how the code works
    Add comments that will describe how major parts of the code work and what they are doing/used for
    
    n/a

--- scripts.js
+++ scripts.js
@@ -6,9 +6,11 @@ const valuedpricedisplay = document.getElementById('tvp');
 const pricepayeddisplay = document.getElementById('tpp');
 
 function render() {
-  tbody.innerText = ''; // This part here will prevent the table from duplicating items already displayed in the table.
+  tbody.innerText = '';
   valuedpricedisplay.innerText = '';
   pricepayeddisplay.innerText = '';
+  // Line 9 will clear the tbody when re-rendering the table in order to prevent any table data from duplicating.
+  // Line 10 and 11 will clear out the displays of the valued price and the payed price when the table is re-rendered.
   gameList.forEach(game => {
     const newRow = template.content.cloneNode(true);
     const tds = newRow.querySelectorAll('td');
@@ -16,15 +18,19 @@ function render() {
     tds[1].innerText = game.systemname;
     tds[2].innerText = `$${game.pricepayed}`;
     tds[3].innerText = `$${game.valuedprice}`;
+    // Line 19 and 20 are using template literals to add the dollar sign before the number when the price values show up on the table. They are stored in the array as just the number.
     tds[4].innerText = game.storename;
     tbody.appendChild(newRow);
+    // Line 23 will apply the new row to the table.
   });
 }
 render();
+// Line 27 is used to display the default "games" in the array on the table when the page loads.
 
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
   gameList.push({
+    // Line 32 through 39 will be used to add what the user inputs on the page into the array. After doing this, it calls for the render function to display the contents of the array including the newly added item on the table.
     gametitle: event.target.elements[0].value,
     systemname: event.target.elements[1].value,
     pricepayed: event.target.elements[2].value,
@@ -50,7 +56,10 @@ document.getElementById('dtvp').addEventListener('click', () => {
   valuedpricedisplay.innerHTML = `$${totalValue}`;
 });
 
+// The two event listeners on Line 43 and 51 will run all the items in the array to get a total specific price and display it to the user with the paragraph tag on the page.
+
 document.getElementById('reset-button').addEventListener('click', () => {
   gameList.length = 0;
+  // Line 62 will empty the contents of the array and thus the table whenever the user clicks the "Reset" button.
   render();
 });
